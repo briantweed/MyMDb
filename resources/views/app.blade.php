d541f8b930df40887bb29d2bb9c3fbbc1cd35ad8
briantweed <brtweed@outlook.com>
2015-12-08 10:40:21 +0000

    swap to bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,30 +5,34 @@
    <head>
       <meta charset="UTF-8">
       <title>@yield('title')</title>
-
-      <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/foundation.min.css') }}" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/bootstrap.css') }}" />
       <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/fontello.css') }}" />
       <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/main.css') }}" />
    </head>
 
    <body>
 
-      <div class="navigation">
-         @yield('navigation')
-      </div>
-
-      <div class="row align-center">
-         <div class="columns no-padding small-11 medium-11 large-12">
-            <h2>@yield('heading')</h2>
-         </div>
-      </div>
-
       <div class="container">
-         @yield('content')
-      </div>
 
-      <script type="text/javascript" src="js/jquery.min.js"></script>
-      @yield('jquery')
+         <div class="navigation">
+            @yield('navigation')
+         </div>
+
+         <div class="row align-center">
+            <div class="col-xs-12">
+               <h2>@yield('heading')</h2>
+            </div>
+         </div>
+
+         <div class="main">
+            @yield('content')
+         </div>
+         
+         <script type="text/javascript" src="js/jquery.min.js"></script>
+         @yield('jquery')
+
+      </div>
 
    </body>
 
--- resources/views/lists/movies/index.blade.php
+++ resources/views/lists/movies/index.blade.php
@@ -15,15 +15,32 @@
 {{-- Page Content --}}
 @section('content')
 
-   <div class="row">
-      @foreach( $movies as $movie )
-         <div class="movie columns small-6 medium-3 large-2">
-            <a href="{{ action('MovieController@show', $movie->movie_id) }}">
-               <img src="{{ URL::asset('images/covers/') }}/{{$movie->cover}}  " /><br/>
-               <i class="ft icon-movie"></i> {{ $movie->name }} ({{ date( 'Y', strtotime( $movie->release_date ) ) }})
-            </a>
+<?php
+   $count = 0;
+?>
+   @foreach( $movies as $movie )
+<?php
+      if($count%6==0) echo '<div class="row">';
+?>
+      <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+         <div class="row margin-bottom">
+            <div class="col-xs-12">
+               <a href="{{ action('MovieController@show', $movie->movie_id) }}">
+                  <img class="img-responsive img-rounded" src="{{asset('images/covers/')}}/{{$movie->cover}}  " />
+               </a>
+            </div>
+            <div class="col-xs-12">
+               <a href="{{ action('MovieController@show', $movie->movie_id) }}">
+                  {{ $movie->name }} ({{date( 'Y', strtotime($movie->release_date))}})
+               </a>
+            </div>
          </div>
-      @endforeach
-   </div>
+      </div>
+<?php
+      if($count%6==5) echo '</div>';
+      $count++;
+?>
+
+   @endforeach
 
 @stop
