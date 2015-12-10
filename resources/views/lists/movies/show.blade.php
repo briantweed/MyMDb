7fea293755d30847085d54b6dcda5e9f9b861999
briantweed <brtweed@outlook.com>
2015-12-10 11:32:17 +0000

    outline buttons

--- resources/views/lists/movies/show.blade.php
+++ resources/views/lists/movies/show.blade.php
@@ -49,7 +49,7 @@
          {{-- delete button --}}
          <div class="row">
             <div class="col-xs-12">
-               <a class="btn btn-danger btn-lg btn-block" href="javascript:void(0);"><i class="ft icon-delete"></i> delete</a>
+               <a class="btn btn-danger-outline btn-lg btn-block" href="javascript:void(0);"><i class="ft icon-delete"></i> delete</a>
             </div>
          </div>
 
