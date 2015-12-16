4f6cc9d28d7221dd9278bc628b4e9da8b6a06f32
briantweed <brtweed@outlook.com>
2015-12-16 15:48:30 +0000

    no more font updates please

--- resources/views/segments/links/keywords.blade.php
+++ resources/views/segments/links/keywords.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ action('KeywordController@index') }}">
-   <i style="font-size:1.5em" class="ft icon-writer"></i>
+   <i style="font-size:1.5em" class="ft icon-tags"></i>
    <span class="hidden-xs"> Keywords</span>
 </a>
