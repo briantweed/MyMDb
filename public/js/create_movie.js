f7c32935baa50b44ab9710fad24c5a29342cc582
briantweed <brtweed@outlook.com>
2015-12-26 10:53:23 +0000

    waiting on rotten tomatoes

--- public/js/create_movie.js
+++ public/js/create_movie.js
@@ -1,43 +1,47 @@
 $(document).ready(function(){
+   
+   $('#movie_studio_id').selectize({
+      create: true
+   });
 
    // apply selctize to dropdowns
    $('#select-movie').selectize({
-    valueField: 'title',
-    labelField: 'title',
-    searchField: 'title',
-    options: [],
-    create: false,
-    render: {
-        option: function(item, escape) {
-            var actors = [];
+      valueField: 'title',
+      labelField: 'title',
+      searchField: 'title',
+      options: [],
+      create: false,
+      render: {
+      option: function(item, escape) {
+         var actors = [];
 
-            return '<div>' +
-                '<span class="title">' +
-                    '<span class="name">' + escape(item.title) + '</span>' +
-                '</span>' +
-                '<span class="description">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
-            '</div>';
-        }
-    },
-    load: function(query, callback) {
-        if (!query.length) return callback();
-        $.ajax({
+         return '<div>' +
+         '<span class="title">' +
+         '<span class="name">' + escape(item.title) + '</span>' +
+         '</span>' +
+         '<span class="description">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
+         '</div>';
+         }
+      },
+      load: function(query, callback) {
+         if (!query.length) return callback();
+         $.ajax({
             url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json',
             type: 'GET',
             dataType: 'jsonp',
             data: {
-                q: query,
-                page_limit: 5,
-                apikey: $('input[name="_tomatoes"]').val()
+               q: query,
+               page_limit: 5,
+               apikey: $('input[name="_tomatoes"]').val()
             },
             error: function() {
-                callback();
+               callback();
             },
             success: function(res) {
-                callback(res.movies);
+               callback(res.movies);
             }
-        });
-    }
-});
+         });
+      }
+   });
 
 });
