511191a2b3d4ef23c4c56f41a7350449dd4448f6
briantweed <brtweed@outlook.com>
2016-02-13 12:27:42 +0000

    link fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,9 +24,9 @@ class UsersTableSeeder extends Seeder
                 'email' => 'brtweed@outlook.com',
                 'password' => '$2y$10$9AKzOHxWzlL3qWWVGibbG.OKeM3oLajo3RLMiFBys2THABAxJfQQK',
                 'level' => 1,
-                'remember_token' => 'sGQV2HFSRqtDHuTpjrhgUciznw1kfE8cyPi5J9rP1sIBfZLO9TUlTjpmFo89',
+                'remember_token' => '54DoOSvy8JfY0bnOivG5EIHrmQvhVj3bkDdhVXGGp1rSv5PhMMd1jJVbQgYj',
                 'created_at' => '0000-00-00 00:00:00',
-                'updated_at' => '2016-02-09 16:10:01',
+                'updated_at' => '2016-02-13 11:27:44',
             ),
         ));
         
