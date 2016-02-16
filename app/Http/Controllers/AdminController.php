7d59c67b6eafcf6acd7e12ee4dfc00a62a3a1d7a
briantweed <brtweed@outlook.com>
2016-02-16 17:12:27 +0000

    no message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 use DB;
 use Request;
 use App\Movies;
+use App\Persons;
 
 use App\Http\Controllers\Controller;
 
@@ -23,6 +24,7 @@ class AdminController extends Controller
       $app = app();
       $data = $app->make('stdClass');
       $data->movie_total = Movies::all()->count();
+      $data->persons_total = Persons::all()->count();
       $data->running_total = Movies::all()->sum('running_time');
       $total_days = floor($data->running_total/1440) == 1 ? floor($data->running_total/1440)." day, " : floor($data->running_total/1440)." days, ";
       $total_hours = floor(($data->running_total%1440)/60) == 1 ? floor(($data->running_total%1440)/60)." hour and " : floor(($data->running_total%1440)/60)." hours and ";
